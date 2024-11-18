2efbddbb6798c4414e0bf0c6f5a1e57d2bef0951
hyoon1129 <hyoon1129@naver.com>
2024-11-18 16:44:13 +0900

    feat: 로그아웃 API 연동

--- public/js/dropdownMenu.js
+++ public/js/dropdownMenu.js
@@ -17,7 +17,21 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-function logout() {
-    alert('로그아웃 되었습니다.');
-    window.location.href = '/';
-}
+const logout = async () => {
+    try {
+        const response = await fetch('http://localhost:8080/auth/logout', {
+            method: 'POST',
+            credentials: 'include',
+        });
+
+        if (response.ok) {
+            alert('로그아웃 되었습니다.');
+            window.location.href = '/';
+        } else {
+            alert('로그아웃 실패: 서버 문제');
+        }
+    } catch (error) {
+        console.error('로그아웃 요청 실패:', error);
+        alert('로그아웃 중 문제가 발생했습니다.');
+    }
+};
