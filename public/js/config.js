8f962a37e91679e8cc9f9f9e0c18cea736a11c83
hyoon1129 <hyoon1129@naver.com>
2024-11-26 14:54:51 +0900

    refactor: ec2 ip 사용

--- public/js/config.js
+++ public/js/config.js
@@ -1,2 +1,2 @@
-// export const API_BASE_URL = 'http://3.35.49.33:8080';
-export const API_BASE_URL = 'http://localhost:8080';
+export const API_BASE_URL = 'http://3.35.49.33:8080';
+// export const API_BASE_URL = 'http://localhost:8080';
