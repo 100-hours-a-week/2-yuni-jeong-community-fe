005238887d413360cf95cef0c775971484c399b2
hyoon1129 <hyoon1129@naver.com>
2025-01-20 10:41:56 +0900

    refactor: API_BASE_URL 환경변수로 불러오게 수정

--- public/js/config.js
+++ public/js/config.js
@@ -1,4 +1,7 @@
-export const API_BASE_URL = 'http://54.180.25.8/api';
-// export const API_BASE_URL = 'http://localhost:8080/api';
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const HOST = process.env.HOST || 'localhost';
+export const API_BASE_URL = `http://${HOST}/api`;
 export const MAX_FILE_SIZE = 5 * 1024 * 1024;
 export const DEFAULT_PROFILE_IMAGE = 'https://dyto0gfaepgcj.cloudfront.net/user-profile.jpg';
