193bc60eda778740de450ca2d8dc37c6d340138a
hyoon1129 <hyoon1129@naver.com>
2025-01-20 16:06:20 +0900

    refactor: 환경변수 수정

--- public/js/config.js
+++ public/js/config.js
@@ -1,7 +1,6 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const HOST = process.env.HOST || 'localhost';
-export const API_BASE_URL = `http://${HOST}/api`;
+export const API_BASE_URL = process.env.BACKEND_URL || 'http://localhost:8080/api';
 export const MAX_FILE_SIZE = 5 * 1024 * 1024;
 export const DEFAULT_PROFILE_IMAGE = 'https://dyto0gfaepgcj.cloudfront.net/user-profile.jpg';
