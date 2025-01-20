69af00f0099d8326332f4da860477e213a35ca7b
hyoon1129 <hyoon1129@naver.com>
2025-01-20 17:15:31 +0900

    feat: webpack 설정

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@ FROM node:21-alpine
 WORKDIR /app
 COPY package.json package-lock.json ./
 RUN npm install
-COPY . .
+COPY ./dist .
 EXPOSE 3000
 CMD ["node", "server.js"]
--- public/js/config.js
+++ public/js/config.js
@@ -1,6 +1,3 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
 export const API_BASE_URL = process.env.BACKEND_URL || 'http://localhost:8080/api';
 export const MAX_FILE_SIZE = 5 * 1024 * 1024;
 export const DEFAULT_PROFILE_IMAGE = 'https://dyto0gfaepgcj.cloudfront.net/user-profile.jpg';
