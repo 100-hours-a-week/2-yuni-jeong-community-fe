7a53d366766b55830c31d3745e67ef5d52f2dc03
hyoon1129 <hyoon1129@naver.com>
2025-02-02 00:37:55 +0900

    fix: api base url 수정

--- public/js/config.js
+++ public/js/config.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = 'http://Community-env.eba-qzp5vchg.ap-northeast-2.elasticbeanstalk.com/api';
+export const API_BASE_URL = 'http://yuniiverse.ap-northeast-2.elasticbeanstalk.com/api';
 export const MAX_FILE_SIZE = 5 * 1024 * 1024;
 export const DEFAULT_PROFILE_IMAGE = 'https://dyto0gfaepgcj.cloudfront.net/user-profile.jpg';
