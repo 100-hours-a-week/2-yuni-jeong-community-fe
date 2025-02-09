87f314d3081d3dbce72580bc07acd354b8a47479
hyoon1129 <hyoon1129@naver.com>
2025-02-09 17:09:03 +0900

    refactor: api url 변경

--- public/js/config.js
+++ public/js/config.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = 'http://13.125.206.249/api';
+export const API_BASE_URL = 'http://3.35.11.227/api';
 export const MAX_FILE_SIZE = 5 * 1024 * 1024;
 export const DEFAULT_PROFILE_IMAGE = 'https://dyto0gfaepgcj.cloudfront.net/user-profile.jpg';
