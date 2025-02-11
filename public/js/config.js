256cf110f5be76af63220987ffcda79938020cba
hyoon1129 <hyoon1129@naver.com>
2025-02-12 02:48:22 +0900

    test: api url 변경

--- public/js/config.js
+++ public/js/config.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = 'http://3.35.11.227/api';
+export const API_BASE_URL = 'http://54.180.86.58/api';
 export const MAX_FILE_SIZE = 5 * 1024 * 1024;
 export const DEFAULT_PROFILE_IMAGE = 'https://dyto0gfaepgcj.cloudfront.net/user-profile.jpg';
